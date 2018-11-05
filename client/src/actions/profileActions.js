279b7369aab8ac435893c73a5b5b0f5cecf74471
AMS777 <alberto@oscuro.es>
2018-11-05 19:21:38 +0100

    Profiles page.

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 import {
   GET_PROFILE,
+  GET_PROFILES,
   PROFILE_LOADING,
   CLEAR_CURRENT_PROFILE,
   GET_ERRORS,
@@ -23,6 +24,24 @@ export const getCurrentProfile = () => dispatch => {
       dispatch({
         type: GET_PROFILE,
         payload: {}
+      })
+    );
+};
+
+// Get profiles
+export const getProfiles = () => dispatch => {
+  axios
+    .get('/api/profile/all')
+    .then(res =>
+      dispatch({
+        type: GET_PROFILES,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_PROFILES,
+        payload: null
       })
     );
 };
